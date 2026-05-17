ca593a051bcffe07369a928ed7b22b3877166895
Carlos-creator <carlos.ramirezval01@gmail.com>
2026-05-17 15:03:29 -0400

    Delete Etapa1 directory

--- Etapa1/src/Territory.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class Territory {
-    private ArrayList<Equipo> equipment = new ArrayList<>();
-
-    public void addEquipment(Equipo eq) {
-        equipment.add(eq);
-    }
-}
