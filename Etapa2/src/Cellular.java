a4eaae05cd205cde3efa56ea11db8ac32cdcdbd3
Carlos-creator <carlos.ramirezval01@gmail.com>
2026-05-17 15:03:39 -0400

    Delete Etapa2 directory

--- Etapa2/src/Cellular.java
+++
@@ -1,5 +0,0 @@
-public class Cellular extends Equipo {
-    public Cellular(String owner, double x, double y, double r, double theta, double dtheta) {
-        super(owner, x, y, r, theta, dtheta);
-    }
-}
