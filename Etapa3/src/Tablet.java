d8fa0be75098b163c053458dd53e288dee82d294
Carlos-creator <carlos.ramirezval01@gmail.com>
2026-05-17 15:03:48 -0400

    Delete Etapa3 directory

--- Etapa3/src/Tablet.java
+++
@@ -1,5 +0,0 @@
-public class Tablet extends EloTelTag {
-    public Tablet(String owner, double x, double y, double r, double theta, double dtheta) {
-        super(owner, "tablet", x, y, r, theta, dtheta);
-    }
-}
